9b578507589774cb49bca96ee9a7779a50ab79a4
edo9300 <edoardo762@gmail.com>
2017-11-06 19:17:14 +0100

    Revert "test2"
    This reverts commit 0a2bcea2a3296c96b993d037c29a3668687b5e6a.

--- AI_core_vs2017solution/ocgcore/processor.cpp
+++ AI_core_vs2017solution/ocgcore/processor.cpp
@@ -126,7 +126,7 @@ int32 field::process() {
 	}
 	case PROCESSOR_SELECT_DISFIELD:
 	case PROCESSOR_SELECT_PLACE: {
-		if (select_place(it->step, it->arg1, it->arg2, it->arg3, (card*)it->ptarget))) {
+		if (select_place(it->step, it->arg1, it->arg2, it->arg3)) {
 			core.units.pop_front();
 			return pduel->bufferlen;
 		} else {
